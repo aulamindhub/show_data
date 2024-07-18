923ca57539467f465ecfa27007947edc213a31f2
nicolascirulli <nicolascirulli2@gmail.com>
2024-07-18 18:13:03 -0300

    deploy

---
+++ dist/assets/main-DYQHF9lq.js
@@ -0,0 +1,5 @@
+import"./style-glcSgW-R.js";document.querySelector("#app").innerHTML=`
+  <div class="h-screen bg-zinc-900 grid place-content-center">
+      <h1 class="text-8xl font-bold text-white">Mindhub</h1>
+  </div>
+`;
